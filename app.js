66236160f36e221fcb79b8bdc2819874d6a3e43a
Rob Scherer <robscherer6@gmail.com>
2023-02-25 13:51:31 -0500

    added return to expose variables for outside function call

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const Gameboard = (() => {
       //create div, add content to div
       boardHTML += `<div class="square" id=square-${index}">${square}</div>`
     })
+    document.querySelector('#gameboard').innerHTML = boardHTML;
   }
-  document.querySelector('#gameboard').innerHTML = boardHTML;
 
   return {
     render,
@@ -19,22 +19,31 @@ const Gameboard = (() => {
 const createPlayer = (name, symbol) => {
   return {
     name,
-    mark
+    symbol
   }
 }
 
 const Game = (() => {
   let players = [];
-  let currentPlayerIndex = 0;
-  let gameOver = false;
+  let currentPlayerIndex;
+  let gameOver;
 
   const start = () => {
-    players =
+    players = [
+      createPlayer(document.querySelector('#player1').value, 'X'),
+      createPlayer(document.querySelector('#player2').value, 'O')
+    ]
+    currentPlayerIndex = 0;
+    gameOver = false;
+    Gameboard.render();
+  }
+  return {
+    start,
   }
 })();
 
 
 const startButton = document.querySelector('#startButton');
 startButton.addEventListener('click', () => {
-  //Game.start();
+  Game.start();
 })
