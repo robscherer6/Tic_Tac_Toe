620be3781b374f4a25fafc7178a86fbd88c29084
Rob Scherer <robscherer6@gmail.com>
2023-02-25 13:41:15 -0500

    working on logic to start game, create players, and display on screen

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
-function Gameboard (array) {
-  this.array = array;
+const Gameboard = (() => {
+  let gameboard = ['', '', '', '', '', '', '', '', ''];
+
+  const render = () => {
+    let boardHTML = '';
+
+    gameboard.forEach((square, index) => {
+      //create div, add content to div
+      boardHTML += `<div class="square" id=square-${index}">${square}</div>`
+    })
+  }
+  document.querySelector('#gameboard').innerHTML = boardHTML;
+
+  return {
+    render,
+  }
+})();
+
+const createPlayer = (name, symbol) => {
+  return {
+    name,
+    mark
+  }
 }
+
+const Game = (() => {
+  let players = [];
+  let currentPlayerIndex = 0;
+  let gameOver = false;
+
+  const start = () => {
+    players =
+  }
+})();
+
+
+const startButton = document.querySelector('#startButton');
+startButton.addEventListener('click', () => {
+  //Game.start();
+})
